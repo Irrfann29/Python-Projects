26956f8159fbadcd251f468759543c2c83a0982b
Irrfann29 <mrirfanhashmi2303@gmail.com>
2025-01-28 21:12:26 +0530

    Minor Changes

--- Rock Paper Scissor/main.py
+++ Rock Paper Scissor/main.py
@@ -1,1 +1,19 @@
-print("hello world")
+import random
+
+choices = ["rock", "paper", "scissors"]
+
+user = input("Enter rock, paper, or scissors: ").lower()
+
+random = random.choice(choices)
+
+print(f"You chose: {user}")
+print(f"Computer chose: {random}")
+
+if user == random:
+    print("It's a tie!")
+elif (user == "rock" and random == "scissors") or \
+     (user == "scissors" and random == "paper") or \
+     (user == "paper" and random == "rock"):
+    print("You win!")
+else:
+    print("You lose!")
