aa0bfb3a0fa883faa60f669c42635db949f8566f
Irrfann29 <mrirfanhashmi2303@gmail.com>
2025-01-29 07:57:54 +0530

    Minor Changes

--- Password Manager/password.py
+++ Password Manager/password.py
@@ -1,1 +1,15 @@
-print("hello world")
+
+while True:
+    password = input("Enter Your Options (View / Add / Remove / Update) Or Press Q to quit :").lower()
+    if password == "Q":
+        break
+    elif password == "view":
+        print("View")
+    elif password == "add":
+        print("Add")
+    elif password == "remove":
+        print("Remove")
+    elif password == "update":
+        print("Update")
+    else:
+        print("Invalid Input")
